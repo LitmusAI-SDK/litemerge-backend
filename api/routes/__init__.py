9d5f4b48981c06cd8d997387f3a333064a435fb8
Devansh Shah <devanshnshah11@gmail.com>
2026-04-07 18:18:51 -0400

    try to fix imports in api.router

--- api/routes/__init__.py
+++ api/routes/__init__.py
@@ -2,3 +2,5 @@ from api.routes.health import router as health
 from api.routes.projects import router as projects
 from api.routes.reports import router as reports
 from api.routes.runs import router as runs
+
+__all__ = ["health", "projects", "reports", "runs"]
